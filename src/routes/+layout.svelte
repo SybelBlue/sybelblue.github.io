200c97bb823ae4c19a3920d42c2688d0fd0e7def
Logan Caraco <lbc.dna@gmail.com>
2025-05-01 16:25:02 -0700

    functioning themes

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+	import '../app.css';
+
+	let { children }: { children?: Snippet<[]> } = $props();
+</script>
+<header>
+  header
+</header>
+<main>
+	{@render children?.()}
+</main>
+
+<style lang="postcss">
+  header {
+    @apply border-2 h-2;
+  }
+  main {
+    @apply absolute overflow-hidden size-full bg-base-100;
+  }
+</style>
